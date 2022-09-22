1d62bdfc7cb1257d056bc6b27c35e58cbbd75073
Sara <stasiek.wypychowski@gmail.com>
2022-09-22 18:09:58 +0200

    quicksort

--- SZKOpulnt/09-2022/20/qsort_from_memory.cpp
+++ SZKOpulnt/09-2022/20/qsort_from_memory.cpp
@@ -1,63 +1,92 @@
-#include<iostream>
-#include<random>
+#include <iostream>
+#include <random>
 
-using std::cin; using std::cout;
+using std::cin;
+using std::cout;
 
-int A[1000];
-
-
-void print_arr(int n){
-    for(int i = 0; i<n; i++){
-        cout <<A[i] <<", ";
+int *make_ranodm_arr(int n, int max)
+{
+    srand(time(NULL));
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        int val = random();
+        arr[i] = val % max - max / 2;
     }
-    cout <<"\n";
+    return arr;
 }
 
-int partition(int p, int k){
-    int pivot = A[p];
-    int pivot_index = p;
-    for(int i = p+1; i<=k; i++){
-        if(A[i]<=pivot){
-            pivot_index++;
+void print_arr(int *arr, int n, int s)
+{
+    for (int i = s; i < s + n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int partition(int arr[], int begin, int end)
+{
+    int pivot = arr[begin];
+
+    int count = 0;
+    for (int i = begin+1; i <= end; i++)
+    {
+        if (arr[i] <= pivot)
+        {
+            count++;
         }
     }
-    std::swap(A[pivot_index-1], A[p]);
+    int pivot_id = begin + count;
+    std::swap(arr[pivot_id], arr[begin]);
 
-    cout <<pivot <<"\n";
-    int marker1 = p, marker2 = k;
-    while(marker1<pivot_index && marker2>pivot_index){
-        while(A[marker1]<=pivot)marker1++;
-        while(A[marker2]>pivot)marker2--;
-        if(A[marker1]>pivot && A[marker2]<=pivot){
-            std::swap(A[marker1], A[marker2]);
+    cout << pivot << " " << pivot_id << "\n";
+    print_arr(arr, end-begin + 1, begin);
+
+    int marker1 = begin, marker2 = end;
+
+    while (marker1 < pivot_id && marker2 > pivot_id)
+    {
+        while (arr[marker1] <= pivot)
+            marker1++;
+        while (arr[marker2] > pivot)
+            marker2--;
+        if (marker1 < pivot_id && marker2 > pivot_id)
+        {
+            std::swap(arr[marker1], arr[marker2]);
             marker1++;
             marker2--;
         }
     }
-    return pivot_index;
-    
+    return pivot_id;
 }
 
-void qsort(int p, int k, int n){
-    if(p>=k) return;
-
-    int pivot = partition(p, k);
-    print_arr(n);
-    qsort(p, pivot, n);
-    print_arr(n);
-    qsort(pivot+1, k, n);
-
-}
-
-int main(){
-    srand(time(NULL));
-    int n;
-    cin>>n;
-    for(int i = 0; i<n; i++){
-        A[i] = random()%20000 - 10000;
+void qsort(int arr[], int front, int end)
+{
+    if (front < end)
+    {
+        int pivot_id = partition(arr, front, end);
+        qsort(arr, front, pivot_id - 1);
+        qsort(arr, pivot_id + 1, end);
     }
-    print_arr(n);
-    qsort(0, n, n);
-    print_arr(n);
+}
+
+int main()
+{
+    srand(time(NULL));
+    int n, a;
+    cin >> n;
+    cin >> a;
+
+    int *arr = make_ranodm_arr(n, a);
+    print_arr(arr, n, 0);
+    qsort(arr, 0, n);
+    // int pivot = partition(arr, 0, n - 1);
+    // print_arr(arr, n, 0);
+    // partition(arr, 0, pivot - 1);
+    // print_arr(arr, n, 0);
+    // partition(arr, pivot + 1, n - 1);
+    print_arr(arr, n, 0);
+
     return 0;
 }
